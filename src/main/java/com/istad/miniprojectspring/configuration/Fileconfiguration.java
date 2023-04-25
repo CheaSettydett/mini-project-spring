8138376996503fd0f8c1902f6d8359cc1f6616d7
CHEA SETTYDETT <cheasettydett681@gmail.com>
2023-04-25 15:44:02 +0700

    update upload photo in card

--- src/main/java/com/istad/miniprojectspring/configuration/Fileconfiguration.java
+++ src/main/java/com/istad/miniprojectspring/configuration/Fileconfiguration.java
@@ -1,9 +1,11 @@
 package com.istad.miniprojectspring.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class Fileconfiguration implements WebMvcConfigurer {
     @Value("${server.path}")
     String serverPath;
